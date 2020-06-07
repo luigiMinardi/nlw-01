0019cd775c540cdadff61abac3d4ca1da661fae9
rocstar3000 <rocs@MacBook-Pro-de-rocstar3000.local>
2020-06-07 18:49:13 +0200

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+//JSX: Sintaxe de XML dentro do JavaScript. (TSX é idem mas no TypeScript).
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
